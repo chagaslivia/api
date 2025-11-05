a1486d17a7dfa17b219bc45779244f19c9377841
Lívia Chagas <liviaalencarchagas08@gmail.com>
2025-11-04 23:46:36 -0300

    Add files via upload
    Card repository

--- src/main/java/br/com/fingestio/api/repository/CardRepository.java
+++ src/main/java/br/com/fingestio/api/repository/CardRepository.java
@@ -1,7 +1,12 @@
 package br.com.fingestio.api.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.fingestio.api.model.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
+
+    List<Card> findByUser_Id(Long userId);
+
 }
